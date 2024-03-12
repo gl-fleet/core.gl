e64665a3c41804e4065d33a1aa1cca49483a7137
gantulgak <gantulgak@riotinto.com>
2024-03-12 18:48:31 +0800

    v0.0.62

--- core/data/ecosystem.config.js
+++ core/data/ecosystem.config.js
@@ -1,16 +1,20 @@
-const { encodeENV } = require('utils')
+const { encodeENV, env: e } = require('utils')
 const pkg = require('./package.json')
 
 const env = encodeENV({
 
     name: pkg.name,
     version: pkg.version,
-    mode: process.env.MODE,
+    mode: e.MODE,
     me: 'master',
-    proxy: process.env.MODE === 'development' ? 'http://localhost:8010' : 'http://139.59.115.158',
+    proxy: e.MODE === 'development' ? 'http://localhost:8010' : 'http://139.59.115.158',
     replication_debug: true,
     sequelize_debug: true,
     pitunnel_token: 'pitunkey_p2S2HPO5qonUyEc1aGxfI4Yo3MaPnhyj',
+    db_name: e.MODE === 'development' ? 'umine_dev' : 'umine',
+    db_host: e.DB_HOST,
+    db_user: e.DB_USER,
+    db_pass: e.DB_PASS,
 
 })
 
--- core/data/src/index.ts
+++ core/data/src/index.ts
@@ -5,12 +5,19 @@ import { decodeENV, Collect, Safe, env, log } from 'utils'
 import { Listener } from './listener'
 import { Persist } from './persist'
 
-const { name, version, mode, me, proxy } = decodeENV()
+const { name, version, mode, me, db_name, db_user, db_pass } = decodeENV()
 log.success(`"${env.npm_package_name}" <${version}> module is running on "${process.pid}" / [${mode}] 🚀🚀🚀\n`)
 
-const cf = {
+console.log(decodeENV())
+
+const cf: any = {
     local: new Host({ name, port: 8040 }),
-    sequelize: new Sequelize({ dialect: 'sqlite', storage: `../../${me}_${name}.sqlite`, logging: false }),
+    // sequelize: new Sequelize({ dialect: 'sqlite', storage: `../../${me}_${name}.sqlite`, logging: false }),
+    sequelize: new Sequelize(db_name, db_user, db_pass, {
+        host: '139.59.115.158',
+        dialect: 'postgres',
+        pool: { max: 16, min: 4, acquire: 30000, idle: 15000 }
+    }),
 }
 
 const run = () => {
@@ -25,6 +32,7 @@ Safe(async () => {
     await cf.sequelize.authenticate()
     run()
     await cf.sequelize.sync({ force: false })
+
     // User.sync() - This creates the table if it doesn't exist (and does nothing if it already exists)
     // User.sync({ force: true }) - This creates the table, dropping it first if it already existed
     // User.sync({ alter: true }) - This checks what is the current state of the table in the database(which columns it has, what are their data types, etc), and then performs the necessary changes in the table to make it match the model.
