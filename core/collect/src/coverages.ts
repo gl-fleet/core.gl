868bf99658bfb18c3eab8213ae2126fcd7bb4391
gantulgak <gantulgak@riotinto.com>
2024-05-31 10:24:38 +0800

    v0.0.97

--- core/collect/src/coverages.ts
+++ core/collect/src/coverages.ts
@@ -49,30 +49,30 @@ export class Coverages {
             updatedAt: { type: DataTypes.STRING, defaultValue: () => Now() },
             deletedAt: { type: DataTypes.STRING, defaultValue: null },
 
-        // }, { indexes: [{ unique: true, fields: ['proj', 'type', 'est', 'nrt'] }] })
+            // }, { indexes: [{ unique: true, fields: ['proj', 'type', 'est', 'nrt'] }] })
         }, {
             indexes: [
                 {
                     unique: false,
-                    name: 'Proj_index',
+                    name: `${this.name}_proj_index`,
                     using: 'BTREE',
                     fields: ['proj'],
                 },
                 {
                     unique: false,
-                    name: 'Type_index',
+                    name: `${this.name}_type_index`,
                     using: 'BTREE',
                     fields: ['type'],
                 },
                 {
                     unique: false,
-                    name: 'Est_index',
+                    name: `${this.name}_est_index`,
                     using: 'BTREE',
                     fields: ['est'],
                 },
                 {
                     unique: false,
-                    name: 'Nrt_index',
+                    name: `${this.name}_nrt_index`,
                     using: 'BTREE',
                     fields: ['nrt'],
                 },
